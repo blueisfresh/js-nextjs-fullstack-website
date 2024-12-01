426c81c2082bad87a5b97014920fa95e0f452f36
blueisfresh <elias.guerematchi.vrabl@gmail.com>
2024-12-01 10:37:56 +0100

    Personalized Blog Posts using API

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 export default async function Page() {
   const response = await fetch("https://dummyjson.com/posts?limit=10");
   const data = await response.json();
 
   return (
-    <main className="text-center pt-32 px-5">
+    <main className="text-center pt-16 px-5">
       <h1 className="text-4xl md:text-5xl font-bold mb-5">All Posts</h1>
 
       <ul>
         {data.posts.map((post) => (
-          <li key={post.id} className="mb-5">
-            <h2 className="text-2xl font-bold">{post.title}</h2>
+          <li key={post.id} className="mb-3">
+            <Link href={`/posts/${post.id}`}>{post.title}</Link>
           </li>
         ))}
       </ul>
